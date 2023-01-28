730043e8b2789c4cddbb590d5a5fb2c51a14cd9c
viruj <virujthakur01@gmail.com>
2023-01-28 22:41:41 +0530

    added home screen and google map object

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,27 @@
+import 'package:aqi_wizard/HomeScreen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  static final CameraPosition _kGooglePlex= const CameraPosition(
+      target: LatLng(20.5937, 78.9629),
+      zoom: 4,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(initialCameraPosition: _kGooglePlex,
+      )
+    );
+  }
+}
